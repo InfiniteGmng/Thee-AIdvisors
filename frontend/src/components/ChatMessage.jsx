08c8a88eda823205b32adef15261ea34f77a8757
Dhiyaa Nazim <dhysdrn@gmail.com>
2025-04-14 12:56:37 -0700

    Changed UI and added dark mode

--- frontend/src/components/ChatMessage.jsx
+++ frontend/src/components/ChatMessage.jsx
@@ -1,7 +1,7 @@
 const ChatMessage = ({ chat }) => {
     return (
         <div className={`message ${chat.role === "model" ? 'bot' : 'user'}-message`}>
-            <p className="message-text">{chat.text}</p>
+            {chat.text}
         </div>
     )
 }
